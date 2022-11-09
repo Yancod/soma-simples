f3c09198968c736a69eddc716589bc67e01c6822
Yancod <yanchanceller@gmail.com>
2022-11-09 00:10:42 -0300

    Criação da logica de soma

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 const priNum = document.getElementById("priNum")
 const segNum = document.getElementById("segNum")
+const btnSoma = document.getElementById("btnSoma")
+const resNum = document.getElementById("resNum")
+
+btnSoma.addEventListener("click", function () {
+    const numberOne = +priNum.value
+    const numbertwo = +segNum.value
+    const somaGeral = numberOne + numbertwo
+    resNum.textContent = somaGeral;
+  });
+
